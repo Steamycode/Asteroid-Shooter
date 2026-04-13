fe22c68ea196dc82132eac661f237d1994f130b0
Steamycode <e.u.regis1@gmail.com>
2026-04-13 18:35:42 +0300

    Update and rename main/main.py to main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ pygame.init()
 WIDTH = 1280
 HEIGHT = 720
 
-bg_img = pygame.image.load('main/assets/sprites/space.png')
-ship_img = pygame.image.load('main/assets/sprites/spaceship.png')
-medium_asteroid_img = pygame.image.load('main/assets/sprites/asteroid.png')
+bg_img = pygame.image.load('assets/sprites/space.png')
+ship_img = pygame.image.load('assets/sprites/spaceship.png')
+medium_asteroid_img = pygame.image.load('assets/sprites/asteroid.png')
 big_asteroid_img = pygame.transform.scale(medium_asteroid_img, (150, 150))
 small_asteroid_img = pygame.transform.scale(medium_asteroid_img, (50, 50))
-bullet_img = pygame.image.load('main/assets/sprites/bullet.png')
+bullet_img = pygame.image.load('assets/sprites/bullet.png')
 
-shoot_sound = pygame.mixer.Sound('main/assets/sounds/laser.mp3')
-explosion_sound = pygame.mixer.Sound('main/assets/sounds/explosion.mp3')
+shoot_sound = pygame.mixer.Sound('assets/sounds/laser.mp3')
+explosion_sound = pygame.mixer.Sound('assets/sounds/explosion.mp3')
 shoot_sound.set_volume(.25)
 explosion_sound.set_volume(.25)
 
