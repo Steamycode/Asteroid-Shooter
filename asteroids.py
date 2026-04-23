969d80dff6f019614444bbb27e0d20fdd413e385
Steamycode <e.u.regis1@gmail.com>
2026-04-23 16:24:30 +0300

    Update asteroids.py

--- asteroids.py
+++ asteroids.py
@@ -1,20 +1,26 @@
 import random
-
 from assets import small_asteroid_img, medium_asteroid_img, big_asteroid_img, WIDTH, HEIGHT
+from game_object import GameObject
 
 
-class Asteroid():
+class Asteroid(GameObject):
     def __init__(self):
         self.rank = 0
         self.image = None
         self.width = 0
         self.height = 0
+        self.set_random_location()
+        self.set_velocity()
+
+    def set_random_location(self):
         self.ran_point = random.choice([
             (random.randrange(0, WIDTH - self.width),
              random.choice([-self.height - 5, HEIGHT + 5])),
             (random.choice([-self.width - 5, WIDTH + 5]),
              random.randrange(0, HEIGHT - self.height))])
         self.x, self.y = self.ran_point
+
+    def set_velocity(self):
         if self.x < WIDTH // 2:
             self.x_direction = 1
         else:
@@ -27,7 +33,9 @@ class Asteroid():
         self.y_velocity = self.y_direction * random.randrange(1, 3)
 
     def draw(self, win):
+        super().draw(win)
         win.blit(self.image, (self.x, self.y))
+
 
 class BigAsteroid(Asteroid):
     def __init__(self):
@@ -46,6 +54,7 @@ class MediumAsteroid(Asteroid):
         self.width = 100
         self.height = 100
 
+
 class SmallAsteroid(Asteroid):
     def __init__(self):
         super().__init__()
@@ -56,9 +65,8 @@ class SmallAsteroid(Asteroid):
 
 
 class AsteroidFactory():
-    
     @staticmethod
-    def createAsteroid(rank):
+    def create_asteroid(rank):
         if rank == 3:
             return BigAsteroid()
         elif rank == 2:
