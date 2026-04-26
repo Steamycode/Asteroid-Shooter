e93a716f65238bc33c3c1d7bae8a12c5b2b13514
Steamycode <e.u.regis1@gmail.com>
2026-04-26 14:05:22 +0300

    Update assets.py

--- assets.py
+++ assets.py
@@ -19,12 +19,10 @@ class AssetManager:
         self._medium_asteroid_img = pygame.image.load('assets/sprites/asteroid.png')
         self._big_asteroid_img = pygame.transform.scale(self._medium_asteroid_img, (150, 150))
         self._small_asteroid_img = pygame.transform.scale(self._medium_asteroid_img, (50, 50))
-        
         self._shoot_sound = pygame.mixer.Sound('assets/sounds/laser.mp3')
         self._explosion_sound = pygame.mixer.Sound('assets/sounds/explosion.mp3')
         self._shoot_sound.set_volume(0.25)
         self._explosion_sound.set_volume(0.25)
-
         self._screen_width = 1280
         self._screen_height = 720
 
