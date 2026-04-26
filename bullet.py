9eaa3b9667488c9527930fe33f3ae4981d142028
Steamycode <e.u.regis1@gmail.com>
2026-04-26 14:11:07 +0300

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,28 +1,31 @@
 import pygame
-from assets import WIDTH, HEIGHT
+from assets import AssetManager
 from game_object import GameObject
 
-pygame.init()
 
 class Bullet(GameObject):
+    
     def __init__(self, x, y, cos, sin):
-        self.x = x
-        self.y = y
-        self.width = 4
-        self.height = 4
-        self.cos = cos
-        self.sin = sin
-        self.x_velocity = self.cos * 10
-        self.y_velocity = self.sin * 10
+        self._x = x
+        self._y = y
+        self._width = 4
+        self._height = 4
+        self._cos = cos
+        self._sin = sin
+        self._x_velocity = self._cos * 10
+        self._y_velocity = self._sin * 10
+        self._assets = AssetManager()
 
     def move(self):
-        self.x += self.x_velocity
-        self.y -= self.y_velocity
+        self._x += self._x_velocity
+        self._y -= self._y_velocity
 
     def draw(self, win):
         super().draw(win)
-        pygame.draw.rect(win, (255, 255, 255), [self.x, self.y, self.width, self.height])
+        pygame.draw.rect(win, (255, 255, 255), [self._x, self._y, self._width, self._height])
 
     def check_off_screen(self):
-        if (self.x < -50 or self.x > WIDTH or self.y > HEIGHT or self.y < -50):
+        if (self._x < -50 or self._x > self._assets._screen_width or 
+            self._y > self._assets._screen_height or self._y < -50):
             return True
+        return False
