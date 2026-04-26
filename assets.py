288c8b0c1dc1f30fbe8d607d801811ec9d74f4a1
Steamycode <e.u.regis1@gmail.com>
2026-04-26 18:50:32 +0300

    Update assets.py

--- assets.py
+++ assets.py
@@ -12,26 +12,29 @@ class AssetManager:
     
     def __init__(self):
 
-        pygame.init()
-        
-        self._bg_img = pygame.image.load('assets/sprites/space.png')
-        self._ship_img = pygame.image.load('assets/sprites/spaceship.png')
-        self._medium_asteroid_img = pygame.image.load('assets/sprites/asteroid.png')
-        self._big_asteroid_img = pygame.transform.scale(self._medium_asteroid_img, (150, 150))
-        self._small_asteroid_img = pygame.transform.scale(self._medium_asteroid_img, (50, 50))
-        self._shoot_sound = pygame.mixer.Sound('assets/sounds/laser.mp3')
-        self._explosion_sound = pygame.mixer.Sound('assets/sounds/explosion.mp3')
-        self._shoot_sound.set_volume(0.25)
-        self._explosion_sound.set_volume(0.25)
-        self._screen_width = 1280
-        self._screen_height = 720
+        if not hasattr(self, "initialised"):
+            pygame.init()
+            
+            self._bg_img = pygame.image.load('assets/sprites/space.png')
+            self._ship_img = pygame.image.load('assets/sprites/spaceship.png')
+            self._medium_asteroid_img = pygame.image.load('assets/sprites/asteroid.png')
+            self._big_asteroid_img = pygame.transform.scale(self.medium_asteroid_img, (150, 150))
+            self._small_asteroid_img = pygame.transform.scale(self.medium_asteroid_img, (50, 50))
+            self._shoot_sound = pygame.mixer.Sound('assets/sounds/laser.mp3')
+            self._explosion_sound = pygame.mixer.Sound('assets/sounds/explosion.mp3')
+            self._shoot_sound.set_volume(0.25)
+            self._explosion_sound.set_volume(0.25)
+            self._screen_width = 1280
+            self._screen_height = 720
+
+            self.initialised = True
 
     @property
-    def WIDTH(self):
+    def screen_width(self):
         return self._screen_width
 
     @property
-    def HEIGHT(self):
+    def screen_height(self):
         return self._screen_height
     
     @property
