be5eb61368c9fdce250fe9cae1280feecdaf3b97
Steamycode <e.u.regis1@gmail.com>
2026-04-26 14:04:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,237 +1,201 @@
 import pygame
 import random
-from assets import WIDTH, HEIGHT, bg_img, shoot_sound, explosion_sound
+from assets import AssetManager
 from asteroids import AsteroidFactory
+from bullet import Bullet
 from player import Player
+from collision import Collision
+from game_state import GameState
 
+pygame.display.set_caption('Asteroid Shooter')
 
-pygame.init()
+class Game:
 
-pygame.display.set_caption('Asteroids')
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-
-class GameState():
     def __init__(self):
-        self.run = True
-        self.gameover = False
-        self.lives = 3
-        self.score = 0
-        self.sound_on = True
-        self._high_score = 0
-        self._count = 0
+        self._assets = AssetManager()
+        self._game_state = GameState()
+        self._asteroids = []
+        self._player = Player()
+        self._collision = Collision()
+        self._asteroid_factory = AsteroidFactory()
+        self._window = pygame.display.set_mode((self._assets._screen_width, self._assets._screen_height))
+        self._clock = pygame.time.Clock()
+        self.load_high_score()
+        self._font = pygame.font.SysFont('calibri', 30)
+        self.update_gui()
 
     @property
-    def count(self):
-        return self._count
+    def asteroids(self):
+        return self._asteroids
     
-    @count.setter
-    def count(self, value):
-        self._count = value
+    @property
+    def player(self):
+        return self._player
+    
+    @property
+    def game_state(self):
+        return self._game_state
+    
+    @property
+    def collision(self):
+        return self._collision
+    
+    @property
+    def assets(self):
+        return self._assets
+    
+    @property
+    def window(self):
+        return self._window
+
+    @property
+    def clock(self):
+        return self._clock
+    
+    @property
+    def asteroid_factory(self):
+        return self._asteroid_factory
+    
+    @property
+    def lives_txt(self):
+        return self._lives_txt
+
+    @property
+    def restart_txt(self):
+        return self._restart_txt
+
+    @property
+    def score_txt(self):
+        return self._score_txt
+
+    @property
+    def high_score_txt(self):
+        return self._high_score_txt
+
+    def load_high_score(self):
+        try:
+            with open('scores.csv', 'r') as score_file:
+                self._game_state._high_score = int(score_file.readline().strip())
+        except (FileNotFoundError, ValueError):
+            print('Error loading scores file')
+            self._game_state._high_score = 0
+
+    def save_high_score(self, score):
+        try:
+            with open('scores.csv', 'w') as score_file:
+                score_file.write(str(score))
+                self._game_state._high_score = score
+        except (FileNotFoundError, ValueError):
+            print(f'Error saving high score: {score}')
+
+    def update_gui(self):
+        self._lives_txt = self._font.render(f'Lives: {self._game_state.lives}', 1, (255, 255, 255))
+        self._restart_txt = self._font.render('Press Tab to Play Again', 1, (255, 255, 255))
+        self._score_txt = self._font.render(f'Score: {self._game_state.score}', 1, (255, 255, 255))
+        self._high_score_txt = self._font.render(f'High Score: {self._game_state.high_score}', 1, (255, 255, 255))
+        self._restart_position = (self._assets._screen_width // 2 - self._restart_txt.get_width() // 2,
+                                  self._assets._screen_height // 2 - self._restart_txt.get_height() // 2)
+        self._high_score_position = (self._assets._screen_width - self._high_score_txt.get_width() - 25,
+                                     35 + self._score_txt.get_height())
+        self._score_position = (self._assets._screen_width - self._score_txt.get_width() - 25, 25)
+
+    def draw_window(self):
+        self._window.blit(self._assets.bg_img, (0, 0))
+        self._player.draw(self._window)
         
-    @property
-    def high_score(self):
-        return self._high_score
-    
-    @high_score.setter
-    def high_score(self, value):
-        self._high_score = value
+        for asteroid in self._asteroids:
+            asteroid.draw(self._window)
 
-    def add_score(self, points):
-        self.score += points
+        for bullet in self._player.bullets:
+            bullet.draw(self._window)
 
-    def lose_life(self):
-        self.lives -= 1
-        if self.sound_on:
-            explosion_sound.play()
-        if self.lives <= 0:
-            self.gameover = True
+        if self._game_state.gameover:
+            self._window.blit(self._restart_txt, self._restart_position)
 
-    def toggle_sound(self):
-        self.sound_on = not self.sound_on
+        self._window.blit(self._score_txt, self._score_position)
+        self._window.blit(self._high_score_txt, self._high_score_position)
+        self._window.blit(self._lives_txt, (25, 25))
+        
+        pygame.display.update()
 
-    def reset(self):
-        self.run = True
-        self.gameover = False
-        self.lives = 3
-        self.score = 0
-        self.sound_on = True
-        self.count = 0
+    def update_game_state(self):
+        self._player.update_location(self._assets._screen_width, self._assets._screen_height)
 
-game_state = GameState()
-asteroids = []
-player = Player()
-
-try:
-    with open('scores.txt', 'r') as fr:
-        game_state.high_score = int(fr.read())
-except (FileNotFoundError, ValueError):
-    print('Error loading scores file')
-    game_state.high_score = 0
-
-
-def draw_window():
-    font = pygame.font.SysFont('arial', 30)
-    lives_txt = font.render('Lives: ' + str(game_state.lives), 1, (255, 255, 255))
-    restart_txt = font.render('Press Tab to Play Again', 1, (255,255,255))
-    score_txt = font.render('Score: ' + str(game_state.score), 1, (255,255,255))
-    high_score_txt = font.render('High Score: ' + str(game_state.high_score), 1, (255, 255, 255))
-
-    restart_position = (WIDTH // 2 - restart_txt.get_width() // 2,
-                        HEIGHT // 2 - restart_txt.get_height() // 2)
-    high_score_position = (WIDTH - high_score_txt.get_width() - 25,
-                           35 + score_txt.get_height())
-    score_position = (WIDTH - score_txt.get_width() - 25, 25)
-
-    window.blit(bg_img, (0, 0))
-    player.draw(window)
-    
-    for asteroid in asteroids:
-        asteroid.draw(window)
-
-    for bullet in player.bullets:
-        bullet.draw(window)
-
-    if game_state.gameover:
-        window.blit(restart_txt, restart_position)
-
-    window.blit(score_txt, score_position)
-    window.blit(high_score_txt, high_score_position)
-    window.blit(lives_txt, (25, 25))
-    
-    pygame.display.update()
-
-
-def handle_bullet_asteroid_collision(bullet, asteroid):
-    if asteroid.rank == 3:
-        if game_state.sound_on:
-            explosion_sound.play()
-        game_state.add_score(10)
-        na1 = AsteroidFactory.create_asteroid(2)
-        na2 = AsteroidFactory.create_asteroid(2)
-        na1.x, na1.y = asteroid.x, asteroid.y
-        na2.x, na2.y = asteroid.x, asteroid.y
-        asteroids.extend([na1, na2])
-    elif asteroid.rank == 2:
-        if game_state.sound_on:
-            explosion_sound.play()
-        game_state.add_score(20)
-        na1 = AsteroidFactory.create_asteroid(1)
-        na2 = AsteroidFactory.create_asteroid(1)
-        na1.x, na1.y = asteroid.x, asteroid.y
-        na2.x, na2.y = asteroid.x, asteroid.y
-        asteroids.extend([na1, na2])
-    else:
-        game_state.add_score(30)
-        if game_state.sound_on:
-            explosion_sound.play()
-
-    asteroids.pop(asteroids.index(asteroid))
-    player.bullets.pop(player.bullets.index(bullet))
-
-
-def check_collisions():
-    for asteroid in asteroids: #list(asteroids): Use a copy to modify list during iteration
-        asteroid.x += asteroid.x_velocity
-        asteroid.y += asteroid.y_velocity
-
-        # Check player collision
-        player_left = player.x - player.width // 2
-        player_right = player.x + player.width // 2
-        player_top = player.y - player.height // 2
-        player_bottom = player.y + player.height // 2
-
-        if ((asteroid.x >= player_left and
-             asteroid.x <= player_right) or
-            ((asteroid.x + asteroid.width) <= player_right and
-             (asteroid.x + asteroid.width) >= player_left)):
-            if ((asteroid.y >= player_top and
-                 asteroid.y <= player_bottom) or
-                (asteroid.y + asteroid.height >= player_top and
-                 asteroid.y + asteroid.height <= player_bottom)):
-                game_state.lose_life()
-                asteroids.pop(asteroids.index(asteroid))
+        for bullet in self._player.bullets:
+            bullet.move()
+            if bullet.check_off_screen():
+                self._player.bullets.pop(self._player.bullets.index(bullet))
+        
+        for asteroid in self._asteroids:
+            asteroid.move()
+            if self._collision._check_player_collision(asteroid, self._player):
+                self._game_state.lose_life()
+                self._asteroids.pop(self._asteroids.index(asteroid))
+                break
+            if self._collision._check_bullet_collision(asteroid, self._asteroids, self._player, self._game_state):
+                self._collision._handle_bullet_asteroid_collision(self._player, bullet, asteroid, self._asteroids, self._game_state)
                 break
 
-        # Check bullet collision
-        for bullet in player.bullets:
-            if ((bullet.x >= asteroid.x and
-                 bullet.x <= asteroid.x + asteroid.width) or
-                (bullet.x + bullet.width >= asteroid.x and
-                 bullet.x + bullet.width <= asteroid.x + asteroid.width)):
-                if ((bullet.y >= asteroid.y and
-                     bullet.y <= asteroid.y + asteroid.height) or
-                    (bullet.y + bullet.height >= asteroid.y and
-                     bullet.y + bullet.height <= asteroid.y + asteroid.height)):
-                    handle_bullet_asteroid_collision(bullet, asteroid)
-                    break
+    def game_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if self._game_state.score > self._game_state.high_score:
+                    self.save_high_score(self._game_state.score)
+                self._game_state.run = False
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not self._game_state.gameover:
+                    self._player.fire(Bullet(self._player._head[0], self._player._head[1], self._player._cos, self._player._sin))
+                    if self._game_state.sound_on:
+                        self._assets.shoot_sound.play()
 
-def update_game_state():
-    player.update_location()
+                if event.key == pygame.K_m:
+                    self._game_state.toggle_sound()
 
-    for bullet in player.bullets:
-        bullet.move()
-        if bullet.check_off_screen():
-            player.bullets.pop(player.bullets.index(bullet))
+                if event.key == pygame.K_TAB:
+                    if self._game_state.gameover:
+                        if self._game_state.score > self._game_state.high_score:
+                            self.save_high_score(self._game_state.score)
+                        self._player._x = self._assets._screen_width // 2
+                        self._player._y = self._assets._screen_height // 2
+                        self._player._angle = 0
+                        self._player.update_rotation(0)
+                        self._game_state.restart()
+                        self._asteroids.clear()
+                        self._player.bullets.clear()
 
-    check_collisions()
+    def handle_player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self._player.turn_left()
+        if keys[pygame.K_RIGHT]:
+            self._player.turn_right()
+        if keys[pygame.K_UP]:
+            self._player.move_forward()
 
+    def spawn_asteroid(self):
+        rank = random.choice([1, 1, 1, 2, 2, 3])
+        new_asteroid = self._asteroid_factory.create_asteroid(rank)
+        self._asteroids.append(new_asteroid)
 
-def game_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            if game_state.score > game_state.high_score:
-                with open('scores.txt', 'w') as fw:
-                    fw.write(str(game_state.score))
-                game_state.high_score = game_state.score
-            game_state.run = False
+    def run(self):
+        while self._game_state.run:
+            self._clock.tick(60)
+            self._game_state.count += 1
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and not game_state.gameover:
-                player.fire()
-                if game_state.sound_on:
-                    shoot_sound.play()
+            if not self._game_state.gameover:
+                if self._game_state.count % 50 == 0:
+                    self.spawn_asteroid()
 
-            if event.key == pygame.K_m:
-                game_state.toggle_sound()
+                self.update_game_state()
+                self.handle_player_input()
 
-            if event.key == pygame.K_TAB:
-                if game_state.score > game_state.high_score:
-                        with open('scores.txt', 'w') as fw:
-                            fw.write(str(game_state.score))
-                        game_state.high_score = game_state.score
-                        
-                if game_state.gameover:
-                    game_state.reset()
-                    asteroids.clear()
-                    player.bullets.clear()
+            self.update_gui()
+            self.game_events()
+            self.draw_window()
 
-
-def handle_player_input():
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player.turn_left()
-    if keys[pygame.K_RIGHT]:
-        player.turn_right()
-    if keys[pygame.K_UP]:
-        player.move_forward()
+        pygame.quit()
 
 if __name__ == '__main__':
-    while game_state.run:
-        clock.tick(60)
-        game_state.count += 1
-
-        if not game_state.gameover:
-            if game_state.count % 50 == 0:
-                rank = random.choice([1, 1, 1, 2, 2, 3])
-                new_asteroid = AsteroidFactory.create_asteroid(rank)
-                asteroids.append(new_asteroid)
-
-            update_game_state()
-            handle_player_input()
-
-        game_events()
-        draw_window()
-
-    pygame.quit()
+    game = Game()
+    game.run()
