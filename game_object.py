7eb39e149cc47550b86c51bc04434b7516af4854
Steamycode <e.u.regis1@gmail.com>
2026-04-26 18:51:54 +0300

    Update game_object.py

--- game_object.py
+++ game_object.py
@@ -5,3 +5,11 @@ class GameObject(ABC):
     @abstractmethod
     def draw(self, win):
         pass
+
+    @abstractmethod
+    def move(self):
+        pass
+
+    @abstractmethod
+    def check_off_screen(self):
+        pass
