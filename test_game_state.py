839ccf2409adc5b2b43be26d2dcbdaa375bd5505
Steamycode <e.u.regis1@gmail.com>
2026-04-26 22:57:23 +0300

    Update test_game_state.py

--- test_game_state.py
+++ test_game_state.py
@@ -1,36 +1,99 @@
-import unittest
-from scripts.game_state import GameState
+from scripts.assets import AssetManager
 
+class GameState:
 
-class TestGameState(unittest.TestCase):
-    
-    def setUp(self):
-        self.game_state = GameState()
-    
-    def test_gamestate_add_score(self):
-        initial_score = self.game_state.score
-        self.game_state.add_score(10)
-        self.assertEqual(self.game_state.score, initial_score + 10)
-    
-    def test_gamestate_lose_life(self):
-        initial_lives = self.game_state.lives
-        self.game_state.lose_life()
-        self.assertEqual(self.game_state.lives, initial_lives - 1)
-    
-    def test_gamestate_lose_all_lives_triggers_gameover(self):
-        self.game_state.lose_life()
-        self.game_state.lose_life()
-        self.game_state.lose_life()
-        self.assertTrue(self.game_state.gameover)
-    
-    def test_gamestate_toggle_sound(self):
-        initial_state = self.game_state.sound_on
-        self.game_state.toggle_sound()
-        self.assertNotEqual(self.game_state.sound_on, initial_state)
-        self.game_state.toggle_sound()
-        self.assertEqual(self.game_state.sound_on, initial_state)
+    def __init__(self):
+        self._run = True
+        self._gameover = False
+        self._first_launch = True
+        self._lives = 3
+        self._score = 0
+        self._sound_on = True
+        self._high_score = 0
+        self._count = 0
 
+    @property
+    def run(self):
+        return self._run
+    
+    @run.setter
+    def run(self, value):
+        self._run = value
+    
+    @property
+    def gameover(self):
+        return self._gameover
+    
+    @gameover.setter
+    def gameover(self, value):
+        self._gameover = value
 
+    @property
+    def first_launch(self):
+        return self._first_launch
+    
+    @first_launch.setter
+    def first_launch(self, value):
+        self._first_launch = value
+    
+    @property
+    def lives(self):
+        return self._lives
+    
+    @lives.setter
+    def lives(self, value):
+        self._lives = value
+    
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, value):
+        self._score = value
 
-if __name__ == '__main__':
-    unittest.main()
+    @property
+    def sound_on(self):
+        return self._sound_on
+    
+    @sound_on.setter
+    def sound_on(self, value):
+        self._sound_on = value
+    
+    @property
+    def count(self):
+        return self._count
+    
+    @count.setter
+    def count(self, value):
+        self._count = value
+        
+    @property
+    def high_score(self):
+        return self._high_score
+    
+    @high_score.setter
+    def high_score(self, value):
+        self._high_score = value
+        
+
+    def add_score(self, points):
+        self.score += points
+
+    def lose_life(self):
+        self.lives -= 1
+        if self.sound_on:
+            assets = AssetManager()
+            assets.explosion_sound.play()
+        if self.lives <= 0:
+            self.gameover = True
+
+    def toggle_sound(self):
+        self.sound_on = not self.sound_on
+
+    def restart(self):
+        self.run = True
+        self.gameover = False
+        self.lives = 3
+        self.score = 0
+        self.count = 0
