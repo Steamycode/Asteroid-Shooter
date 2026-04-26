48fdfbe2365c51e30459de010f71d40934a354e2
Steamycode <e.u.regis1@gmail.com>
2026-04-26 14:12:53 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,66 +1,80 @@
 import pygame
 import math
-from assets import ship_img, WIDTH, HEIGHT
-from bullet import Bullet
+from assets import AssetManager
 from game_object import GameObject
 
-pygame.init()
 
 class Player(GameObject):
+    
     def __init__(self):
-        self.x = WIDTH // 2
-        self.y = HEIGHT // 2
-        self.img = ship_img
-        self.width = self.img.get_width()
-        self.height = self.img.get_height()
-        self.angle = 0
-        self.rotated = pygame.transform.rotate(self.img, self.angle)
-        self.rotated_rect = self.rotated.get_rect(center = (self.x, self.y))
+        self._assets = AssetManager()
+        self._x = self._assets._screen_width // 2
+        self._y = self._assets._screen_height // 2
+        self._img = self._assets.ship_img
+        self._width = self._img.get_width()
+        self._height = self._img.get_height()
+        self._angle = 0
+        self._rotated = pygame.transform.rotate(self._img, self._angle)
+        self._rotated_rect = self._rotated.get_rect(center=(self._x, self._y))
+        self._bullets = []
         self.update_direction()
-        self.bullets = []
+
+    @property
+    def width(self):
+        return self._width
+    
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def bullets(self):
+        return self._bullets
+    
+    @property
+    def angle(self):
+        return self._angle
 
     def draw(self, win):
         super().draw(win)
-        win.blit(self.rotated, self.rotated_rect)
+        win.blit(self._rotated, self._rotated_rect)
 
-    def fire(self):
-        new_bullet = Bullet(self.head[0], self.head[1], self.cos, self.sin)
-        self.bullets.append(new_bullet)
+    def fire(self, bullet):
+        self._bullets.append(bullet)
 
     def turn_left(self):
-        self.angle += 5
-        self.update_rotation()
+        self.update_rotation(5)
 
     def turn_right(self):
-        self.angle -= 5
-        self.update_rotation()
+        self.update_rotation(-5)
 
     def move_forward(self):
-        self.x += self.cos * 6
-        self.y -= self.sin * 6
+        self._x += self._cos * 6
+        self._y -= self._sin * 6
         self.update_position()
 
-    def update_rotation(self):
-        self.rotated = pygame.transform.rotate(self.img, self.angle)
-        self.rotated_rect = self.rotated.get_rect(center=(self.x, self.y))
+    def update_rotation(self, angle):
+        self._angle += angle
+        self._rotated = pygame.transform.rotate(self._img, self._angle)
+        self._rotated_rect = self._rotated.get_rect(center=(self._x, self._y))
         self.update_direction()
 
     def update_position(self):
-        self.rotated_rect = self.rotated.get_rect(center=(self.x, self.y))
+        self._rotated_rect = self._rotated.get_rect(center=(self._x, self._y))
         self.update_direction()
 
     def update_direction(self):
-        self.cos = math.cos(math.radians(self.angle + 90))
-        self.sin = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cos * self.width // 2,
-                     self.y - self.sin * self.height // 2)
+        self._cos = math.cos(math.radians(self._angle + 90))
+        self._sin = math.sin(math.radians(self._angle + 90))
+        self._head = (self._x + self._cos * self._width // 2,
+                      self._y - self._sin * self._height // 2)
 
-    def update_location(self):
-        if self.x > WIDTH + 50:
-            self.x = 0
-        elif self.x < -self.width:
-            self.x = WIDTH
-        if self.y > HEIGHT + 50:
-            self.y = HEIGHT
-        elif self.y < -self.height:
-            self.y = HEIGHT
+    def update_location(self, width, height):
+        if self._x > width + 50:
+            self._x = 0
+        elif self._x < -50:
+            self._x = width
+        if self._y > height + 50:
+            self._y = 0
+        elif self._y < -50:
+            self._y = height
