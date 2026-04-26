13b831b5987f6d26ea06ceacc7d1a5052fde8ab4
Steamycode <e.u.regis1@gmail.com>
2026-04-26 18:52:08 +0300

    Update game_state.py

--- game_state.py
+++ game_state.py
@@ -9,7 +9,6 @@ class GameState:
         self._score = 0
         self._sound_on = True
         self._high_score = 0
-        self._high_score_name = ""
         self._count = 0
 
     @property
@@ -32,13 +31,25 @@ class GameState:
     def lives(self):
         return self._lives
     
+    @lives.setter
+    def lives(self, value):
+        self._lives = value
+    
     @property
     def score(self):
         return self._score
     
+    @score.setter
+    def score(self, value):
+        self._score = value
+
     @property
     def sound_on(self):
         return self._sound_on
+    
+    @sound_on.setter
+    def sound_on(self, value):
+        self._sound_on = value
     
     @property
     def count(self):
@@ -55,25 +66,26 @@ class GameState:
     @high_score.setter
     def high_score(self, value):
         self._high_score = value
+        
 
     def add_score(self, points):
-        self._score += points
+        self.score += points
 
     def lose_life(self):
-        self._lives -= 1
-        if self._sound_on:
+        self.lives -= 1
+        if self.sound_on:
             assets = AssetManager()
             assets.explosion_sound.play()
-        if self._lives <= 0:
-            self._gameover = True
+        if self.lives <= 0:
+            self.gameover = True
 
     def toggle_sound(self):
-        self._sound_on = not self._sound_on
+        self.sound_on = not self.sound_on
 
     def restart(self):
-        self._run = True
-        self._gameover = False
-        self._lives = 3
-        self._score = 0
-        self._sound_on = True
-        self._count = 0
+        self.run = True
+        self.gameover = False
+        self.lives = 3
+        self.score = 0
+        self.sound_on = True
+        self.count = 0
